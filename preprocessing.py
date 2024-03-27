9c4d9a7993b07d834cfd3bfc8a67a8a9acdfea2c
František Kmječ <frantisek.kmjec@gmail.com>
2024-03-27 22:12:54 +0100

    Add basic data preprocessing, Lasso regression and some visualization

---
+++ preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer, make_column_transformer
+
+# assumes heart failures have been put in without the heart_disease label column
+def preprocess_hf_for_lasso(heart_failures: pd.DataFrame):
+    categorical = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
+    numerical = ["Age", "RestingBP", "Cholesterol", "FastingBS", "MaxHR" , "Oldpeak"]
+    preprocessed = heart_failures.copy()
+
+    numeric_transformer = Pipeline(steps=[
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps=[
+        # ('labelencoder', LabelEncoder()),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numerical),
+            ('cat', categorical_transformer, categorical)
+        ])
+    preprocessor.fit(preprocessed)
+    preprocessed = preprocessor.fit_transform(preprocessed)
+    return preprocessed
+
