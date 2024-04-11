35bc844adb0fce73ccb92122e65bbf56fc5a5380
František Kmječ <frantisek.kmjec@gmail.com>
2024-04-11 20:33:09 +0200

    Add working NAM

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
 
+PREPROCESSED_COLUMN_NAMES = ['Age', 'Sex - F', 'Sex - M', 'ChestPainType - ASY', 'ChestPainType - ATA', 'ChestPainType - NAP', 'ChestPainType - TA', 'RestingBP', 'Cholesterol', 'FastingBS',
+       'RestingECG - LVH', 'RestingECG - Normal', 'RestingECG - ST', 'MaxHR', 'ExerciseAngina - N', 'ExerciseAngina - Y', 'Oldpeak', 'ST_Slope - Down', 'ST_Slope - Flat',  'ST_Slope - Up',]
+
 # assumes heart failures have been put in without the heart_disease label column
 def preprocess_hf_for_lasso(heart_failures: pd.DataFrame):
     categorical = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
@@ -27,3 +30,9 @@ def preprocess_hf_for_lasso(heart_failures: pd.DataFrame):
     preprocessed = preprocessor.fit_transform(preprocessed)
     return preprocessed
 
+def preprocess_hf_for_nam(heart_failures: pd.DataFrame):
+    labels = heart_failures.pop("HeartDisease")
+    preprocessed = preprocess_hf_for_lasso(heart_failures)
+    # pd_preprocessed = pd.DataFrame(preprocessed, columns=PREPROCESSED_COLUMN_NAMES)
+    return preprocessed, labels
+    
